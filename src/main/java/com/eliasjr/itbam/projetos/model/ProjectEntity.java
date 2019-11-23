d85737209acc88a792fe699bfd878e7d850e0ed1
eliasjr <elias.dvlpr@gmail.com>
2019-11-22 21:34:00 -0400

    auto increment

--- src/main/java/com/eliasjr/itbam/projetos/model/ProjectEntity.java
+++ src/main/java/com/eliasjr/itbam/projetos/model/ProjectEntity.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 public class ProjectEntity {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
 	@Column(name = "nome")
